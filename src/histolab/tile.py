d5ea0667c2a42c0b775a120e959cb7e2e7631be7
kheffah <mtageld@emory.edu>
2021-08-07 11:12:58 -0500

    Allow masking out markers in tile

--- src/histolab/tile.py
+++ src/histolab/tile.py
@@ -25,6 +25,7 @@ import PIL
 
 from .filters import image_filters as imf
 from .filters.compositions import FiltersComposition
+from .filters.image_filters_functional import maskout_markers
 from .types import CoordinatePair
 from .util import lazyproperty
 
@@ -42,10 +43,19 @@ class Tile:
         Level of tile extraction, by default 0
     """
 
-    def __init__(self, image: PIL.Image.Image, coords: CoordinatePair, level: int = 0):
+    def __init__(
+        self,
+        image: PIL.Image.Image,
+        coords: CoordinatePair,
+        level: int = 0,
+        mpp: float = None,
+        filter_tissue=True
+    ):
         self._image = image
         self._coords = coords
         self._level = level
+        self._mpp = mpp
+        self._filter_tissue = filter_tissue
 
     def apply_filters(
         self,
@@ -160,6 +170,24 @@ class Tile:
 
         Path(path).parent.mkdir(parents=True, exist_ok=True)
         self._image.save(path)
+
+    @lazyproperty
+    def _tissue_mask(self) -> float:
+        """Return the tissue mask for this tile.
+
+        Returns
+        -------
+        np.array
+            Boolean array of where tissue is.
+        """
+        if self._filter_tissue:
+            filters = FiltersComposition(Tile).tissue_mask_filters
+            tissue_mask = filters(self._image)
+        else:
+            tissue_mask = None
+        tissue_mask, _ = maskout_markers(
+            self._image, tissue_mask, blue=True, green=True, red=False)
+        return tissue_mask
 
     @lazyproperty
     def tissue_ratio(self) -> float:
